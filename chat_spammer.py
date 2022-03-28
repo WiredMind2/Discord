ad0d2b1f44dd413ff6c06a2c6accf860e331e7af
WiredMind <william@michaud.re>
2022-03-28 18:57:34 +0400

    Removed helium files

--- chat_spammer.py
+++ chat_spammer.py
@@ -2,7 +2,7 @@ import time
 import string
 import random
 import threading
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 from account_stealer import User
@@ -15,9 +15,10 @@ def send_msg(msg, thread=False):
 	if thread:
 		return threading.Thread(target=send_msg, args=(msg, False)).start()
 
-	msg = {
-		'content': msg
-	}
+	if isinstance(msg, str):
+		msg = {
+			'content': msg
+		}
 	try:
 		with write_lock:
 			m = user.createMessage(channel=channel, content=msg)
@@ -36,18 +37,26 @@ if __name__ == "__main__":
 	token = will_i_am
 	user = User(token)
 
-	guild = 724303646443044995  # TITANIUM
+	# guild = 724303646443044995  # TITANIUM
 	# channel = 944888647080116224  # global-chat
-	channel = 945569239576764426 # spam
+	# channel = 945569239576764426 # spam
 
-	mode = "msg"
+	guild = 946832192531734579 #adii x lyn x william
+	channel = 957717058169217094 # william-is-lazy-aka...
+
+	mode = "timestamp"
+	i = 1
+	first = time.time()
 	while True:
 		if mode == "timestamp":
 			msg = {
 				'content': datetime.now().isoformat()
 			}
 			send_msg(msg)
-			time.sleep(30)
+			next_timestamp = first + (i * 60)
+			sleep_time = max(0, next_timestamp - time.time())
+			time.sleep(sleep_time)
+			i += 1
 		if mode == "msg":
 			msg = {
 				'content': """/// An asynchronous function that completes quickly. async fn quick_task() -> Result<&'static str> { println!("START quick_task"); await!(delay(10)).context("delay failed")?; println!("END quick_task"); Ok("quick_task result") } /// An asynchronous function that completes very slowly. async fn slow_task() -> Result<&'static str> { println!("START slow_task"); await!(delay(10_000)).context("delay failed")?; println!("END slow_task"); Ok("slow_task result") }"""
