3b8c8e43789929772f67bf975517ffab1e37e318
WiredMind <william@michaud.re>
2022-05-15 22:19:47 +0400

    Updated chat_spammer - multiple accounts are now possible

--- chat_spammer.py
+++ chat_spammer.py
@@ -1,19 +1,18 @@
 import time
 import string
-import random
 import threading
-from datetime import datetime, timedelta
+from datetime import datetime
 
 
-from account_stealer import User
+from user_api import User
 from token_secret import *
 
 write_lock = threading.Lock()
 
-def send_msg(msg, thread=False):
+def send_msg(user, msg, thread=False):
 	global write_lock
 	if thread:
-		return threading.Thread(target=send_msg, args=(msg, False)).start()
+		return threading.Thread(target=send_msg, args=(user, msg, False)).start()
 
 	if isinstance(msg, str):
 		msg = {
@@ -34,15 +33,15 @@ def send_msg(msg, thread=False):
 		print("Error:", e)
 
 if __name__ == "__main__":
-	token = will_i_am
-	user = User(token)
+	tokens = [will_i_am, adithiii]
+	users = [User(token) for token in tokens if token is not None]
 
-	# guild = 724303646443044995  # TITANIUM
+	guild = 724303646443044995  # TITANIUM
 	# channel = 944888647080116224  # global-chat
-	# channel = 945569239576764426 # spam
+	channel = 945569239576764426 # spam
 
-	guild = 946832192531734579 #adii x lyn x william
-	channel = 957717058169217094 # william-is-lazy-aka...
+	# guild = 946832192531734579 #adii x lyn x william
+	# channel = 957717058169217094 # william-is-lazy-aka...
 
 	mode = "timestamp"
 	i = 1
@@ -52,18 +51,21 @@ if __name__ == "__main__":
 			msg = {
 				'content': datetime.now().isoformat()
 			}
-			send_msg(msg)
-			next_timestamp = first + (i * 60)
+			for user in users:
+				send_msg(user, msg)
+			next_timestamp = first + (i * 30)
 			sleep_time = max(0, next_timestamp - time.time())
 			time.sleep(sleep_time)
 			i += 1
 		if mode == "msg":
 			msg = {
-				'content': """/// An asynchronous function that completes quickly. async fn quick_task() -> Result<&'static str> { println!("START quick_task"); await!(delay(10)).context("delay failed")?; println!("END quick_task"); Ok("quick_task result") } /// An asynchronous function that completes very slowly. async fn slow_task() -> Result<&'static str> { println!("START slow_task"); await!(delay(10_000)).context("delay failed")?; println!("END slow_task"); Ok("slow_task result") }"""
+				'content': ":parrot:"
 			}
-			send_msg(msg)
+			for user in users:
+				send_msg(user, msg)
 			time.sleep(1)
 		if mode == "alphabet":
 			for l in string.ascii_lowercase:
-				send_msg(l, True)
+				for user in users:
+					send_msg(user, l, True)
 				time.sleep(1)
